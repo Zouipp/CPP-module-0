25505d74cc79ce82fd08e7899fd3a2ca82571c59
Zouipp <felix.benech@gmail.com>
2026-07-13 02:27:51 +0200

    adding the ex00 nearly done

--- CPP_05/ex00/Bureaucrat.cpp
+++ CPP_05/ex00/Bureaucrat.cpp
@@ -19,7 +19,52 @@ Bureaucrat::~Bureaucrat(void)
 	std::cout << "Default destructor has been called !" << std::endl;
 }
 
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{
+	if (this != &other)
+	{
+		this->grade = other.grade;
+		this->name = other.name;
+	}
+	return *this;
+}
+
 Bureaucrat& Bureaucrat::operator<<(const Bureaucrat& other)
 {
-	
+	std::cout << this->name << ", bureaucrat grade " << this->grade << std::en
+}
+
+int Bureaucrat::getGrade()
+{
+	return (this->grade);
+}
+
+std::string Bureaucrat::getName()
+{
+	return (this->name);
+}
+
+void Bureaucrat::increaseGrade()
+{
+	if (grade - 1 <= 0)
+		throw GradeTooHighException;
+	this->grade--;
+	std::cout << this->name << " has been promoted to #" << this->grade << " !" << std::endl;
+}
+
+void Bureaucrat::decreaseGrade()
+{
+	if (this->grade + 1 > 150)
+		throw GradeTooLowException;
+	this->grade++;
+	std::cout << this->name << " has been demoted to #" << this->grade << " !" << std::endl;
+}
+
+void Bureaucrat::GradeTooHighException()
+{
+	std::cout << "Unvalid grade, is Top 1 not enough ?" << std::endl;
+}
+void  Bureaucrat::GradeTooLowException()
+{
+	std::cout << "I'm sorry you're already the last one, but you can still rise !" << std::endl;
 }
--- CPP_05/ex00/Bureaucrat.hpp
+++ CPP_05/ex00/Bureaucrat.hpp
@@ -1,3 +1,6 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
 #include <string>
 #include <iostream>
 
@@ -7,8 +10,17 @@ class Bureaucrat
 		std::string name;
 		int grade;
 	public :
+		int getGrade();
 		~Bureaucrat(void);
+		void increaseGrade();
+		void decreaseGrade();
+		std::string getName();
+		void GradeTooLowException();
+		void GradeTooHighException();
 		Bureaucrat(const Bureaucrat& other);
 		Bureaucrat(std::string name, int grade);
+		Bureaucrat& operator=(const Bureaucrat& other);
 		Bureaucrat& operator<<(const Bureaucrat& other);
-}
+};
+
+#endif
